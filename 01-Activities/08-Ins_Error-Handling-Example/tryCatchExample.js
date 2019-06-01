060d12b515b083069ad96f6828f361587446fb27
Britta <polished.athena@gmail.com>
2019-06-01 11:18:24 -0500

    updated boiler plate

--- 01-Activities/08-Ins_Error-Handling-Example/tryCatchExample.js
+++
@@ -1,8 +0,0 @@
-// Get filename from user
-var filename = process.argv[2];
-var readFile = require("./helpers").readFile;
-
-// readFile throws due to an unknown error with the filesystem.
-readFile(filename);
-
-console.log("The rest of your program didn't work.");
--- 01-Activities/10-Ins_Mocha-Chai-Example/index.js
+++
@@ -1,8 +0,0 @@
-var multiply = function(x, y) {
-  if (typeof x !== "number" || typeof y !== "number") {
-    throw new Error("x or y is not a number.");
-  }
-  else return x * y;
-};
-
-module.exports = multiply;
