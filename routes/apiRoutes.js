e880f203dde736d7f5920e6d967a487fc1d175a4
alongkot <nathankc94@gmail.com>
2019-06-08 18:08:52 -0500

    seperate models' files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,6 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all projectExamples
-  app.get("/api/projectExamples", function(req, res) {
-    db.projectExamples.findAll({}).then(function(dbprojectExamples) {
-      res.json(dbprojectExamples);
-      
-    });
-  });
-  // Get all library lists
-  app.get("/api/libraryLists", function(req, res) {
-    db.libraryLists.findAll({}).then(function(dblibraryLists) {
-      res.json(dblibraryLists);
-      
-    });
-  });
    // Get all api Lists
    app.get("/api/apiLists", function(req, res) {
     db.apiLists.findAll({}).then(function(dbapiLists) {
@@ -23,17 +9,17 @@ module.exports = function(app) {
   });
 
 
-  // Create a new projectExample
-  app.post("/api/projectExample", function(req, res) {
-    db.projectExample.create(req.body).then(function(dbprojectExample) {
-      res.json(dbprojectExample);
+  // Create a new apiLists
+  app.post("/api/apiLists", function(req, res) {
+    db.apiLists.create(req.body).then(function(dbapiLists) {
+      res.json(dbapiLists);
     });
   });
 
-  // Delete an projectExample by id
-  app.delete("/api/projectExample/:id", function(req, res) {
-    db.projectExample.destroy({ where: { id: req.params.id } }).then(function(dbprojectExample) {
-      res.json(dbprojectExample);
-    });
-  });
+  // Delete an apiLists by id
+  // app.delete("/api/apiLists/:id", function(req, res) {
+  //   db.apiLists.destroy({ where: { id: req.params.id } }).then(function(dbapiLists) {
+  //     res.json(dbapiLists);
+  //   });
+  // });
 };
