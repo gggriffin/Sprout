25a88ea62a225583279bf8424b2c4f165cceb59c
npg311 <47582847+npg311@users.noreply.github.com>
2019-07-13 15:11:36 -0500

    working user OAuth

---
+++ models/users.js
@@ -0,0 +1,34 @@
+module.exports = function (sequelize, DataTypes) {
+  var apiLists = sequelize.define("users", {
+    id: {
+      type: DataTypes.STRING(75),
+      allowNull: false,
+      validate: {
+        len: [1]
+      },
+      primaryKey: true
+    },
+    provider: {
+      type: DataTypes.STRING(75),
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    displayName: {
+      type: DataTypes.STRING(75),
+      allowNull: true,
+      validate: {
+        len: [1]
+      }
+    },
+    photo: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  return apiLists;
+};
