8662098f596f949e9ba888914aa1dea86939f323
Nicholas Golden <npg311@gmail.com>
2019-07-27 12:55:40 -0500

    routes paths and logic for userProfile page

---
+++ controllers/userProfile.js
@@ -0,0 +1,29 @@
+const db = require('../models');
+
+const create = async (req, res) => {
+  res.status(501).json({ msg: 'Not Implemented Yet.' });
+};
+
+const findAll = async (req, res) => {
+  res.status(501).json({ msg: 'Not Implemented Yet.' });
+};
+
+const findById = async (req, res) => {
+  res.status(501).json({ msg: 'Not Implemented Yet.' });
+};
+
+const updateById = async (req, res) => {
+  res.status(501).json({ msg: 'Not Implemented Yet.' });
+};
+
+const deleteById = async (req, res) => {
+  res.status(501).json({ msg: 'Not Implemented Yet.' });
+};
+
+module.exports = {
+  create,
+  findAll,
+  findById,
+  updateById,
+  deleteById,
+};
