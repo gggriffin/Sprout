ffeb97aba0d9adc8d005d545b5c8f327fa0ef95a
Garrett Griffin <griffin.garrett@gmail.com>
2019-07-30 17:19:32 -0500

    connected post route to allow submission of new apis/libraries

--- client/src/Components/Utils/API.js
+++ client/src/Components/Utils/API.js
@@ -2,17 +2,26 @@ import axios from "axios";
 
 export default {
   // Gets all books
-  getLibraries: function() {
-    return axios.get('/api/v1/libraries');
-  },
   getApis: function() {
     return axios.get('/api/v1/apis')
   },
-  voteLibrary: function(id) {
-    return axios.put('/api/v1/libraries/score/' + id);
+  addApi: function(apiData) {
+    return axios.post('api/v1/apis', apiData);
   },
   voteApi: function(id) {
     return axios.put('api/v1/apis/score/' + id);
+  },
+
+
+  getLibraries: function() {
+    return axios.get('/api/v1/libraries');
+  },
+  addLibrary: function(libraryData) {
+    return axios.post('api/v1/libraries', libraryData)
+  },
+  voteLibrary: function(id) {
+    return axios.put('/api/v1/libraries/score/' + id);
   }
+  
 
 };
