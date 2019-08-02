ceb89de641ab09c013e041a51ce113726fcbbe99
Nicholas Golden <npg311@gmail.com>
2019-08-01 19:10:03 -0500

    changed client/server.js chosen port

--- client/public/server.js
+++ client/public/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
